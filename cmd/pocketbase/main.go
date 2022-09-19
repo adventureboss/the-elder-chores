fed740b75e901af79734d47183fa635a66a7aafa
Stephen Adams <stephen.adams@redhat.com>
2022-09-19 10:31:05 -0400

    [task] Added pocketbase and simple endpoint
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

---
+++ cmd/pocketbase/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/hello",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "lubdub")
+			},
+		})
+
+		return nil
+	})
+
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
