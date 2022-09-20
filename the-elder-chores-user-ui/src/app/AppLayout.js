7ee595a1f229741423dd9421ebea37db8435fdef
Stephen <stephen.adams@redhat.com>
2022-09-20 12:39:58 -0400

    task: add a logo banner (#6)
    Add a banner
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- the-elder-chores-user-ui/src/app/AppLayout.js
+++ the-elder-chores-user-ui/src/app/AppLayout.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import {
-    Flex, FlexItem,
-    Masthead,
-    MastheadContent,
-    MastheadMain,
-    Page, PageSection, PageSectionVariants, Stack, StackItem, Text, TextContent, TextVariants,
-    Toolbar,
-    ToolbarContent, ToolbarItem
+  Flex, FlexItem,
+  Masthead,
+  MastheadContent,
+  MastheadMain,
+  Page, PageSection, PageSectionVariants, Stack, StackItem, Text, TextContent, TextVariants,
+  Toolbar,
+  ToolbarContent, ToolbarItem
 } from "@patternfly/react-core";
 import { NavLink } from 'react-router-dom';
+import logo from '../img/logo.png'
 
 const homePath = "/";
 const dashPath = "/dashboard";
@@ -16,42 +17,38 @@ const shopPath = "/shop";
 const questsPath = "/quests";
 
 const Header = () => (
-    <Masthead>
-      <MastheadMain>
-        <MastheadContent>
-          <Toolbar>
-            <ToolbarContent>
-              <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
-            </ToolbarContent>
-          </Toolbar>
-        </MastheadContent>
-      </MastheadMain>
-    </Masthead>
-  );
+  <Masthead>
+    <MastheadMain>
+      <MastheadContent>
+        <Toolbar>
+          <ToolbarContent>
+            <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
+            <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
+            <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
+            <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
+          </ToolbarContent>
+        </Toolbar>
+      </MastheadContent>
+    </MastheadMain>
+  </Masthead>
+);
 
 const AppLayout = ({ children }) => {
   return (
     <React.Fragment>
-        <Flex>
-            <FlexItem style={{marginLeft: 'auto', marginRight: 'auto'}}>
-                <TextContent>
-                    <Text component={ TextVariants.h1 }>
-                        The Elder Chores
-                    </Text>
-                </TextContent>
-            </FlexItem>
-        </Flex>
-      <Page header={ <Header /> }>
-          <PageSection variant={ PageSectionVariants.dark }>
-              <Stack style={{alignItems: "center"}}>
-                  <StackItem>
-                      {children}
-                  </StackItem>
-              </Stack>
-          </PageSection>
+      <Flex>
+        <FlexItem style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+          <img src={logo} alt="The Elder Chores" />
+        </FlexItem>
+      </Flex>
+      <Page header={<Header />}>
+        <PageSection variant={PageSectionVariants.dark}>
+          <Stack style={{ alignItems: "center" }}>
+            <StackItem>
+              {children}
+            </StackItem>
+          </Stack>
+        </PageSection>
       </Page>
     </React.Fragment>
   );
