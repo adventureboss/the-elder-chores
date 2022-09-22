4f3af90452397fbf5b3efa9919fb0c0b669edf8f
Stephen <stephen.adams@redhat.com>
2022-09-22 16:20:25 -0400

    style: add a logo to the masthead (#22)
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- the-elder-chores-user-ui/src/app/AppLayout.js
+++ the-elder-chores-user-ui/src/app/AppLayout.js
@@ -5,6 +5,7 @@ import {
   Masthead,
   MastheadContent,
   MastheadMain,
+  MastheadBrand,
   Page, PageSection, PageSectionVariants, Stack, StackItem,
   Toolbar,
   ToolbarContent, ToolbarItem
@@ -12,32 +13,36 @@ import {
 import { NavLink } from 'react-router-dom';
 import logo from '../img/logo.png'
 import Login from '../pages/Login/Login';
-import {usePocketbase} from "../components/Pocketbase";
+import { usePocketbase } from "../components/Pocketbase";
+import broom from '../img/broom.png'
 
 const homePath = "/";
 const dashPath = "/dashboard";
 const shopPath = "/shop";
 const questsPath = "/quests";
 
-const Header = ({user}) => {
+const Header = ({ user }) => {
   const client = usePocketbase();
 
   return (
-      <Masthead>
-        <MastheadMain>
-          <MastheadContent>
-            <Toolbar>
-              <ToolbarContent>
-                <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
-                <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
-                <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
-                <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
-                {user && <ToolbarItem><Button onClick={() => client.authStore.clear()} variant={ButtonVariant.link}>Logout</Button></ToolbarItem>}
-              </ToolbarContent>
-            </Toolbar>
-          </MastheadContent>
-        </MastheadMain>
-      </Masthead>
+    <Masthead>
+      <MastheadMain>
+        <MastheadBrand>
+          <img src={broom} alt="Elder Chores Logo" />
+        </MastheadBrand>
+        <MastheadContent>
+          <Toolbar>
+            <ToolbarContent>
+              <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
+              <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
+              <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
+              <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
+              {user && <ToolbarItem><Button onClick={() => client.authStore.clear()} variant={ButtonVariant.link}>Logout</Button></ToolbarItem>}
+            </ToolbarContent>
+          </Toolbar>
+        </MastheadContent>
+      </MastheadMain>
+    </Masthead>
   )
 };
 
@@ -50,7 +55,7 @@ const AppLayout = ({ user, children }) => {
           <img src={logo} alt="The Elder Chores" />
         </FlexItem>
       </Flex>
-      <Page header={<Header user={ user } />}>
+      <Page header={<Header user={user} />}>
         <PageSection variant={PageSectionVariants.dark}>
           <Stack style={{ alignItems: "center" }}>
             <StackItem>
