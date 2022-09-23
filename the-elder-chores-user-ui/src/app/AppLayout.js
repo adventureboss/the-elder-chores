3fa4543b3aede2b4cd939e0607414b1c4de3a8ac
Fabricia Diniz <agomesdi@redhat.com>
2022-09-23 02:51:44 +0100

    updates login page

--- the-elder-chores-user-ui/src/app/AppLayout.js
+++ the-elder-chores-user-ui/src/app/AppLayout.js
@@ -24,29 +24,60 @@ const questsPath = "/quests";
 const Header = ({ user }) => {
   const client = usePocketbase();
 
+  // return (
+  //   <Masthead>
+  //     <MastheadMain>
+  //       <MastheadBrand>
+  //         <img src={broom} alt="Elder Chores Logo" />
+  //       </MastheadBrand>
+  //       <MastheadContent>
+  //         <Toolbar>
+  //           <ToolbarContent>
+  //             <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
+  //             <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
+  //             <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
+  //             <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
+  //             {user && <ToolbarItem><Button onClick={() => client.authStore.clear()} variant={ButtonVariant.link}>Logout</Button></ToolbarItem>}
+  //           </ToolbarContent>
+  //         </Toolbar>
+  //       </MastheadContent>
+  //     </MastheadMain>
+  //   </Masthead>
+  // )
+
   return (
-    <Masthead>
-      <MastheadMain>
-        <MastheadBrand>
-          <img src={broom} alt="Elder Chores Logo" />
-        </MastheadBrand>
-        <MastheadContent>
-          <Toolbar>
-            <ToolbarContent>
-              <ToolbarItem><NavLink to={homePath}>Home</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={shopPath}>Shop</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={questsPath}>Quests</NavLink></ToolbarItem>
-              <ToolbarItem><NavLink to={dashPath}>Restricted area</NavLink></ToolbarItem>
-              {user && <ToolbarItem><Button onClick={() => client.authStore.clear()} variant={ButtonVariant.link}>Logout</Button></ToolbarItem>}
-            </ToolbarContent>
-          </Toolbar>
-        </MastheadContent>
-      </MastheadMain>
-    </Masthead>
+    <div id="header">
+      <ul className='display-flex'>
+        <li className='item'><NavLink to={homePath}>Home</NavLink></li>
+        <li className='item'><NavLink to={questsPath}>Quests</NavLink></li>
+        <li className='item'><NavLink to={shopPath}>Shop</NavLink></li>
+        <li className='item-long'><NavLink to={dashPath}>Restricted area</NavLink></li>
+        {user && <li className='item'><Button onClick={() => client.authStore.clear()} variant={ButtonVariant.link}>Logout</Button></li>}
+      </ul>
+    </div>
   )
 };
 
 const AppLayout = ({ user, children }) => {
+
+  // return (
+  //   <React.Fragment>
+  //     <Flex>
+  //       <FlexItem style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+  //         <img src={logo} alt="The Elder Chores" />
+  //       </FlexItem>
+  //     </Flex>
+  //     <Page header={<Header user={ user } />}>
+  //       <PageSection variant={PageSectionVariants.dark}>
+  //         <Stack style={{ alignItems: "center" }}>
+  //           <StackItem>
+  //             {user ? children : <Login />}
+  //           </StackItem>
+  //         </Stack>
+  //       </PageSection>
+  //     </Page>
+  //   </React.Fragment>
+  // );
 
   return (
     <React.Fragment>
@@ -55,15 +86,12 @@ const AppLayout = ({ user, children }) => {
           <img src={logo} alt="The Elder Chores" />
         </FlexItem>
       </Flex>
-      <Page header={<Header user={user} />}>
-        <PageSection variant={PageSectionVariants.dark}>
-          <Stack style={{ alignItems: "center" }}>
-            <StackItem>
-              {user ? children : <Login />}
-            </StackItem>
-          </Stack>
-        </PageSection>
-      </Page>
+      <div>
+        <Header />
+        <div>
+          {user ? children : <Login />}
+        </div>
+      </div>
     </React.Fragment>
   );
 }
